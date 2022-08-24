c5a44d00903a2cab6cb76bb6d4a07c87a8c7eac7
Songmu <y.songmu@gmail.com>
2022-08-24 22:01:09 +0900

    separate out tagging function

--- rcpr.go
+++ rcpr.go
@@ -99,6 +99,9 @@ func newRcpr(ctx context.Context, c *commander) (*rcpr, error) {
 }
 
 func isRcpr(pr *github.PullRequest) bool {
+	if pr == nil {
+		return false
+	}
 	for _, label := range pr.Labels {
 		if label.GetName() == autoLableName {
 			return true
@@ -177,87 +180,13 @@ func Run(ctx context.Context, argv []string, outStream, errStream io.Writer) err
 		rp.c.Git("config", "--local", "user.name", gitUser)
 	}
 
-	{
-		// tag and exit if the HEAD is the merged rcpr
-		commitish, _, err := rp.c.GitE("rev-parse", "HEAD")
+	// If the latest commit is a merge commit of the pull request by rcpr,
+	// tag the semver to the commit and create a release and exit.
+	if pr, err := rp.latestPullRequest(ctx); err != nil || isRcpr(pr) {
 		if err != nil {
 			return err
 		}
-		pulls, _, err := rp.gh.PullRequests.ListPullRequestsWithCommit(
-			ctx, rp.owner, rp.repo, commitish, nil)
-		if err != nil {
-			return err
-		}
-		if len(pulls) > 0 && isRcpr(pulls[0]) {
-			var vfile string
-			if rp.cfg.versionFile == nil {
-				rp.c.Git("checkout", "HEAD~")
-				vfile, err = detectVersionFile(".", currVer)
-				if err != nil {
-					return err
-				}
-				rp.c.Git("checkout", releaseBranch)
-			} else {
-				vfile = rp.cfg.versionFile.String()
-			}
-
-			var nextTag string
-			if vfile != "" {
-				nextVer, err := retrieveVersionFromFile(vfile, currVer.vPrefix)
-				if err != nil {
-					return err
-				}
-				nextTag = nextVer.Tag()
-			} else {
-				nextTag = guessNextSemver(currVer, pulls[0]).Tag()
-			}
-			previousTag := &latestSemverTag
-			if *previousTag == "" {
-				previousTag = nil
-			}
-
-			// To avoid putting pull requests created by rcpr itself in the release notes,
-			// we generate release notes in advance.
-			// Get the previous commitish to avoid picking up the merge of the pull
-			// request made by rcpr.
-			targetCommitish, _, err := rp.c.GitE("rev-parse", "HEAD~")
-			if err != nil {
-				return nil
-			}
-			releases, _, err := rp.gh.Repositories.GenerateReleaseNotes(
-				ctx, rp.owner, rp.repo, &github.GenerateNotesOptions{
-					TagName:         nextTag,
-					PreviousTagName: previousTag,
-					TargetCommitish: &targetCommitish,
-				})
-			if err != nil {
-				return err
-			}
-
-			rp.c.Git("tag", nextTag)
-			if rp.c.err != nil {
-				return rp.c.err
-			}
-			_, _, err = rp.c.GitE("push", "--tags")
-			if err != nil {
-				return err
-			}
-
-			// Don't use GenerateReleaseNote flag and use pre generated one
-			_, _, err = rp.gh.Repositories.CreateRelease(
-				ctx, rp.owner, rp.repo, &github.RepositoryRelease{
-					TagName:         &nextTag,
-					TargetCommitish: &releaseBranch,
-					Name:            &releases.Name,
-					Body:            &releases.Body,
-					// I want to make it as a draft release by default, but it is difficult to get a draft release
-					// from another tool via API, and there is no tool supports it, so I will make it as a normal
-					// release. In the future, there may be an option to create it as a Draft, or conversely,
-					// an option not to create a release.
-					// Draft: github.Bool(true),
-				})
-			return err
-		}
+		return rp.tagRelease(ctx, pr, currVer, latestSemverTag)
 	}
 
 	rcBranch := fmt.Sprintf("rcpr-from-%s", currVer.Tag())
