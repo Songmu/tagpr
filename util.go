21ff579a30b47818ab91a7a48756753ac0b3a970
Songmu <y.songmu@gmail.com>
2022-10-15 13:19:44 +0900

    define tp.setOutput

--- util.go
+++ util.go
@@ -1,8 +1,16 @@
 package tagpr
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func exists(filename string) bool {
 	_, err := os.Stat(filename)
 	return err == nil
+}
+
+func (tp *tagpr) setOutput(name, value string) error {
+	_, err := fmt.Fprintf(tp.out, "::set-output name=%s::%s\n", name, value)
+	return err
 }
